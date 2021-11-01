8c6a5bc5c7c687e0d2c11ca889094de7a26711a3
ControlledChaos0 <jade.spoon2003@gmail.com>
2021-11-01 15:33:18 -0400

    twitter bot

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         '0sM7MPaY782cLecQ4uN0gX6q3',
+  consumer_secret:      'mctqM7P9VA2zmZxnXVZH94QZPds00mRELBOnvFa3FTawy9jL3y',
+  access_token:         '1453723926605938692-NhDC6vPAyPxy8tQ0Bx5e53ip6oHc10',
+  access_token_secret:  'JZDuoatunG9WWnwRBWofRVEf1cdsBRPhqcdDtJ72ZdzC1'
 }
